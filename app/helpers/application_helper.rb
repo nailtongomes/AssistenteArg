e12a583efb0f134b5548feeb1163f2cb00206657
Nailton Gomes <nailtongomes@ig.com.br>
2014-11-13 14:42:39 -0300

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,18 @@ module ApplicationHelper
   def textilize(text)
     RedCloth.new(text).to_html.html_safe
   end   
+  
+  def language_links
+    links = []
+    I18n.available_locales.each do |locale|
+      locale_key = "translation.#{locale}"
+      if locale == I18n.locale
+        links << link_to(I18n.t(locale_key), "#", class: "btn disabled")
+      else
+        links << link_to(I18n.t(locale_key), url_for(locale: locale.to_s), class: "btn")
+      end
+    end
+    links.join("\n").html_safe
+  end
+  
 end
