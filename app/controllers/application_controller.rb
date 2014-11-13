ce8338bd1e1b59a560c96b0cb64995bf0bf55f85
Nailton Gomes <nailtongomes@ig.com.br>
2014-11-13 14:40:18 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+  before_filter :set_locale_from_url
+  
   include SessionsHelper
 
   rescue_from ActiveRecord::RecordNotFound, :with => :rescue_action_in_public
@@ -14,4 +18,13 @@ class ApplicationController < ActionController::Base
     super
     end
   end
+  
+  def set_locale
+    if lang = request.env['HTTP_ACCEPT_LANGUAGE']
+      lang = lang[/^[a-z]{2}/]
+      lang = :"pt-BR" if lang == "pt"
+    end
+    I18n.locale = params[:locale] || lang || I18n.default_locale
+  end
+  
 end
